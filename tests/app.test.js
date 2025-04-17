0b874494eb35966d53b45e9e9bb95f3a1ca52c85
Adhnan190 <adhanan.kuthpudeen@relanto.ai>
2025-04-17 13:41:52 +0530

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,9 @@
 const { addNumbers } = require('../public/script.js');
 
-// Using Jest for testing
-describe('Addition Function', () => {
-    test('properly adds two numbers', () => {
-        expect(addNumbers(1, 2)).toBe(3);
-        expect(addNumbers(5, 7)).toBe(12);
-        expect(addNumbers(-1, 1)).toBe(0);
-    });
+test('adds 5 + 7 to equal 12', () => {
+  expect(add(5, 7)).toBe(12);
+});
+
+test('adds -1 + 1 to equal 0', () => {
+  expect(add(-1, 1)).toBe(0);
 });
